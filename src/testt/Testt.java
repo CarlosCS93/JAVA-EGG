12f97cda56b96b09275ae449813fbdb9e27cf937
Carlos Chugcho Silva <carloschugcho1992@gmail.com>
2022-12-13 22:27:14 -0500

    Primer Commit

---
+++ src/testt/Testt.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testt;
+//Funcion Scanner
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Testt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        int num1=4;
+        int num2=7;
+        int suma=num1+num2;
+        System.out.println("El resultado de la suma es = "+suma);
+        //Ejercicio 4
+        String nombre = "Carlos";
+        int edad = 29;
+        System.out.println(nombre + edad) ;
+        
+        //Ejemplo Scanner ingresar informacion por teclado
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Con esta clase guardamos valores ingresados por consola en las variables");
+        System.out.println("Ingresa tu nombre");
+        String name=leer.nextLine();
+                       
+        System.out.println("Ingresa tu edad");
+        int edadd = leer.nextInt();
+        //Ejercicio 5 Scanner
+        //Define una variable de tipo boolean, double y char. Guarda información en ellas a través del Scanner.
+        System.out.println("Ingrese un numero decimal");
+        double deci = leer.nextDouble();
+        
+        //DETECCION DE ERRORES
+        
+        Scanner mensaje = new Scanner(System.in);
+        System.out.println("Ingresa tu edad");
+        int edaddd = mensaje.nextInt();
+
+        System.out.println("Ingresa tu nombre");
+        String nombres = mensaje.nextLine();
+        
+        
+        
+        
+                
+    }
+    
+}
